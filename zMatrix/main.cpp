00ce9a6341b1cb1317107b82602cfa024db1b802
ffiirree <1603209884@qq.com>
2016-09-16 20:36:19 +0800

    sobel算子

--- zMatrix/main.cpp
+++ zMatrix/main.cpp
@@ -16,12 +16,28 @@ using namespace z;
 
 int main(int argc, char *argv[])
 {
-	Matrix8u img = imread("test.jpeg");
-	cv::imshow("read", cv::Mat(img));
+	Matrix8u gray, mdis, img = imread("test.jpeg");
+	cv::Mat display, src = img;
 
-	Matrix8u gray;
+	// zMatrix 读取的原图
+	cv::imshow("z org", cv::Mat(img));
+
+	// zMatrix 的sobel算子边缘检测效果
 	cvtColor(img, gray, BGR2GRAY);
-	cv::imshow("gray", cv::Mat(gray));
+	sobel(gray, mdis, 1, 1);
+	cv::imshow("z soble", cv::Mat(mdis));
+
+	// openCV中的sobel算子边缘检测效果
+	cv::Sobel(src, display, CV_8U, 1, 1);
+	cv::imshow("cv sobel", display);
+
+	// zMatrix 中的Canny边缘检测效果
+	Canny(gray, mdis, 3, 6);
+	cv::imshow("z Canny", cv::Mat(mdis));
+
+	// openCV中的Canny边缘检测效果
+	cv::Canny(src, display, 30, 60);
+	cv::imshow("cv Canny", display);
 
  	cv::waitKey(0);
 	return 0;
