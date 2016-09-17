c9581de3ac94016737104a9c0f78f0519dc186b3
ffiirree <1603209884@qq.com>
2016-09-17 13:21:23 +0800

    Kinect

--- zMatrix/main.cpp
+++ zMatrix/main.cpp
@@ -2,8 +2,6 @@
 #include <opencv2\core.hpp>
 #include <opencv2\highgui\highgui.hpp>
 #include <opencv2\imgproc\imgproc.hpp>
-#include <string>
-#include <ctime>  
 
 #include "zcore.h"
 #include "zimgproc\zimgproc.h"
@@ -17,26 +15,27 @@ using namespace z;
 
 int main(int argc, char *argv[])
 {
-	KinectSensor kinect(FrameTypes_Color);
-	cv::Mat depthImg48, depthImg24(424, 512, CV_8U), IRImg24(424, 512, CV_8U);
-	char key = 1;
+	KinectSensor kinect(FrameTypes_All);
+	cv::Mat depthImg16, depthImg8(424, 512, CV_8U), IRImg8(424, 512, CV_8U);
+	char key = 0;
+
 	while (key != ' ') {
-		if (kinect.isNewFrameArrived(FrameTypes_Color) == S_OK) {
-			if (SUCCEEDED(kinect.update(FrameTypes_Color))) {
+		if (kinect.isNewFrameArrived(FrameTypes_All) == S_OK) {
+			if (SUCCEEDED(kinect.update(FrameTypes_All))) {
 
 				// 显示彩色图像
 				imshow("color", kinect.getColorImg());
 
-				//// 显示深度图像，像素位数为48位
-				//depthImg48 = kinect.getDepthImg();
-				//cout << ((USHORT *)depthImg48.data)[512 / 2 + 512 * (424 / 2)] << endl;
-				//depthImg48.convertTo(depthImg24, CV_8U, 255.0f / kinect.getDepthMaxReliableDistance());
-				//circle(depthImg24, cv::Point(512 / 2, 424 / 2), 10, cv::Scalar(0, 255, 0));
-				//imshow("depth", depthImg24);
+				// 显示深度图像，像素位数为16位
+				depthImg16 = kinect.getDepthImg();
+				cout << ((USHORT *)depthImg16.data)[512 / 2 + 512 * (424 / 2)] << endl;
+				depthImg16.convertTo(depthImg8, CV_8U, 255.0f / kinect.getDepthMaxReliableDistance());
+				circle(depthImg8, cv::Point(512 / 2, 424 / 2), 10, cv::Scalar(0, 255, 0));
+				cv::imshow("depth", depthImg8);
 
-				//// 显示红外图像
-				//kinect.getInfraImg().convertTo(IRImg24, CV_8U, 1.0 / 256.0);
-				//cv::imshow("infra", IRImg24);
+				// 显示红外图像
+				kinect.getInfraImg().convertTo(IRImg8, CV_8U, 1.0 / 256.0);
+				cv::imshow("infra", IRImg8);
 
 				key = cv::waitKey(10);
 			}
